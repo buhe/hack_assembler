f86879268ab82b3afd852c18e2dc81b929cad4eb
buhe <bugu1986@126.com>
2022-05-19 12:50:18 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    input: Option<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let input = &cli.input.unwrap();
+    let src = fs::read_to_string(input).expect("src not existed.");
+    println!("src:\n{}", src);
+}
